59e77f09b49f2b1e19445b53bf5108fb015deafe
WatcharapolxFa <watcharapolyotadee@outlook.com>
2020-09-22 04:06:12 +0700

    master

--- T_OnlyWhen_F/Source.cpp
+++ T_OnlyWhen_F/Source.cpp
@@ -42,44 +42,6 @@ void outputchar()
 
 	printf("Output T or F : ");
 
-	//^
-	if (str.Letters[1] == '^')
-	{
-		if (str.Letters[0] == 'T' && str.Letters[2] == 'T' )
-		{
-			printf("T");
-		}
-		else
-		{
-			printf("F");
-		}
-	}
-
-	//V
-	if (str.Letters[1] == 'V')
-	{
-		if (str.Letters[0] == 'F' && str.Letters[2] == 'F')
-		{
-			printf("F");
-		}
-		else
-		{
-			printf("T");
-		}
-	}
-
-	//>
-	if (str.Letters[1] == '>')
-	{
-		if (str.Letters[0] == 'T' && str.Letters[2] == 'F' )
-		{
-			printf("F");
-		}
-		else
-		{
-			printf("T");
-		}
-	}
 
 	//<
 	if (str.Letters[1] == '<')
@@ -87,7 +49,7 @@ void outputchar()
 		if (str.Letters[0] == 'T' && str.Letters[2] == 'T' || str.Letters[0] == 'F' && str.Letters[2] == 'F')
 		{
 			printf("T");
-		}
+		} 
 		else
 		{
 			printf("F");
